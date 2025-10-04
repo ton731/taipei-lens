6348ead580f3a15f8ae76e63a2a6a1fd9d18646b
tony <ton731@gmail.com>
2025-10-04 11:19:45 +0800

    更新 vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: true,
+  publicDir: 'public',
+  server: {
+    host: '0.0.0.0',
+    port: 10000,
     strictPort: true,
-  },
+    allowedHosts: ['https://taipei-lens.onrender.com', 'localhost']
+  }
 })
